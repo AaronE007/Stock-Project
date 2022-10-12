0bed658b6ec904c2a88ca88392c9231322c6833d
Aaron Elias <aaronelias007@gmail.com>
2022-10-11 21:10:29 -0400

    Rewrite sessions create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,14 @@ class Api::SessionsController < ApplicationController
   skip_before_action :authorize, only: :create
 
   def create
-    user = User.find_by(username: params[:username])
+    
+    user = User.find_by_username(params[:username])
+
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      render json: user
-    else
-      render json: { errors: ["Invalid username or password"] }, status: :unauthorized
-    end
-  end
-
-  def destroy
-    session.delete :user_id
-    head :no_content
-  end
-
-end
+      render json: UserSerializer.new(user), status: :accepted
+    else 
+      render json: { errors: ["Invalid Username or Password"] }, status: :unauthorized
+  end 
+  
+end 
