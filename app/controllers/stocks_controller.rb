e903bb5a24796557c11b2a68d28c1ac24bb11456
Aaron Elias <aaronelias007@gmail.com>
2022-10-13 21:02:44 -0400

    Finish logic for Stock controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,3 +1,19 @@
 class StocksController < ApplicationController
   
+  def index
+    render json: Stock.all  
+  end 
+
+  def create
+    stock = current_user.stocks.create!(stock_params)
+    render json: stock, status: :created
+  end
+
+  private 
+
+  def recipe_params
+    params.permit(:name, :price_purchased_at, :number, :info)
+  end 
+
+
 end
