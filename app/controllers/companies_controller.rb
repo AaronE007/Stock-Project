7bb0b08e5d063b48738a424cc6eee579867e14a5
Aaron Elias <aaronelias007@gmail.com>
2022-10-13 21:42:22 -0400

    Add create and show to Company controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,3 +1,19 @@
 class CompaniesController < ApplicationController
   
+  def create
+    stock = current_user.companies.create!(companies_params)
+    render json: company, status: :created
+  end
+
+  def show 
+    render json: @companies
+  end
+
+  
+  private 
+
+  def companies_params
+    params.permit(:name, :description)
+  end
+
 end
