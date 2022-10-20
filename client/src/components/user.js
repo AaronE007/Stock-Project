6490985f1123adc1d3992a117057fcd9bc9dc0c2
Aaron Elias <aaronelias007@gmail.com>
2022-10-19 22:54:11 -0400

    Delete styled components

--- client/src/components/user.js
+++ client/src/components/user.js
@@ -1,7 +1,15 @@
 import React, {useState, useContext, useEffect} from "react";
 
+const UserContext = React.createContext();
 
-export default function user() {
+
+function UserProvider({ children }) {
+  const [user, setUser] = useState({});
+  const [loggedIn, setLoggedIn] = useContext(false)
+
+  useEffect(() => {
+    fetch()
+  })
   return (
     <div>
       
--- client/src/index.js
+++ client/src/index.js
@@ -2,28 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
-import { createGlobalStyle } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
-  *,
-  *::before, 
-  *::after {
-    box-sizing: border-box;
-  }
 
-  html, body {
-    margin: 0;
-  }
-
-  body {
-    font-family: BlinkMacSystemFont,-apple-system,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Fira Sans","Droid Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
-  }
-`;
 
 ReactDOM.render(
-  <BrowserRouter>
-    <GlobalStyle />
-    <App />
-  </BrowserRouter>,
+  <UserProvider>
+    <BrowserRouter>
+      <GlobalStyle />
+        <App />
+    </BrowserRouter>
+  </UserProvider>,
   document.getElementById("root")
 );
