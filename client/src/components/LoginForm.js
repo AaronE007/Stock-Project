c292e9781e31dca1534e7239871458ef9dc4fce0
Aaron Elias <aaronelias007@gmail.com>
2022-10-20 21:47:45 -0400

    Add simple login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -6,13 +6,19 @@ function LoginForm({ onLogin }) {
   const [errors, setErrors] = useState([]);
 
   function handleSubmit(e) {
-   
+   e.preventDefault()
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      
-      
+      <label>Username: </label>
+      <input onChange={(e) => setUsername(e.target.value)} type="text" id="name" value={username} required/>
+
+      <br/>
+      <br/>
+
+      <label>Password: </label>
+      <input onChange={(e) => setPassword(e.target.value)} type="password" id="password" value={password} required/>
     </form>
   );
 }
