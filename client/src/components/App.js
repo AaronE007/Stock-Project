022a902a6ceb6d07d1d4109594c4d88543bdca52
Aaron Elias <aaronelias007@gmail.com>
2022-10-20 20:14:59 -0400

    Delete obsolete state from app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,22 +7,10 @@ import NewRecipe from "../pages/NewRecipe";
 import 
 
 function App() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    // auto-login
-    fetch("/api/me").then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user));
-      }
-    });
-  }, []);
-
-  if (!user) return <Login onLogin={setUser} />;
-
+  
   return (
     <>
-      <NavBar user={user} setUser={setUser} />
+      <NavBar  />
       <main>
         <Routes>
           <Route path="/new">
