41b346ea447ac108a85b9c81e126966b73fb866a
Aaron Elias <aaronelias007@gmail.com>
2022-10-20 20:31:25 -0400

    Add sign in and sign up routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,27 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import Login from "../pages/Login";
 import RecipeList from "../pages/RecipeList";
 import NewRecipe from "../pages/NewRecipe";
-import 
+import Home from "./Home";
 
 function App() {
   
   return (
-    <>
+    <div>
       <NavBar  />
       <main>
         <Routes>
-          <Route path="/new">
-            <NewRecipe user={user} />
+          <Route path="/new"> 
           </Route>
           <Route path="/">
-            <RecipeList />
+          <Home />
+          </Route>
+          <Route path="/">
+          <Home />
+          </Route>
+          <Route path="/logout">
+          <Home />
+          </Route>
+          <Route path="/login">
+          <Home />
+          </Route>
+          <Route path="/signup">
+          <Home />
+          </Route>
+          <Route path="/">
+            <Home />
           </Route>
         </Routes>
       </main>
-    </>
+    </div>
   );
 }
 
